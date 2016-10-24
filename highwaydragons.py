b627ebc808da0c8d717e97819b435536f2e459d0
Alex Hughes <alex@customsoftuk.com>
2016-10-24 14:18:42 +0200

    Framework
    Added a base with contact and members pages.  Both pages are place
    holders only.

--- highwaydragons.py
+++ highwaydragons.py
@@ -26,42 +26,13 @@ from datetime import datetime, date
 def index():
     return render_template('index.html')
 
-@app.route('/admin/login', methods=['GET','POST'])
-def login():
-    if request.method == 'POST':
-        store = Store(user=request.form.get('username'))
-        if store.doc:
-            if store.doc['password'] == request.form.get('password'):
-                session['academy'] = [store.key,
-                                      datetime.strftime(date.today(), '%Y%m%d')]
-                return redirect('/')
-            flash('Password not recognised')
-        else:
-            flash('Username not recognised')
-    return render_template('signin.html')
+@app.route('/members')
+def members():
+    return render_template('members.html')
 
-@app.route('/admin/logout', methods=['GET','POST'])
-def logout():
-    if 'hwdragons' in session:
-        del session['hwdragons']
-    return redirect('/admin/login')
-
-@app.route('/admin', methods=['GET','POST'])
-def admin():
-    ref_id, last_date = session.get('hwdragons',(None,None))
-    if not ref_id or last_date != str(date.taday()):
-        del session['hwdragons']
-        return redirect(url_for('login'))
-    if request.method == 'POST':
-        for fname in ('templates','static'):
-            file = request.files[fname]
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(fname, filename))
-            
-    
-
-    return render_template('admin.html')
+@app.route('/contact-us')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
